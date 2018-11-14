cc40751a217862322ad546afcb5a207b45521a2c
P1dum <P1dum@users.noreply.github.com>
2018-11-14 11:47:26 +0100

    Last working main sisi

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,5 +1,3 @@
-//#include "TimerOne.h"
-
 void setup(){
 pinMode(7, OUTPUT);          // pins safe mode
 Serial.begin(9600);
@@ -13,16 +11,21 @@ void wait_us(int us){
         delayMicroseconds(us);
 }
 
+int send_start(){               //Send 18ms pull down signal
 
-
-void send_start(){               //Send 18ms pull down signal
 
   int watchdog = 0;
   
     pinMode(7, OUTPUT);          // Set pin to output mode
+    digitalWrite(7, HIGH); 
+    delay(20); 
     digitalWrite(7, LOW);       // Set pull down signal
-    delay(18);                  // wait for 18ms
-    pinMode(7, INPUT);          // Set pin to safe input mode
+    delay(20);                  // wait for 18ms
+       digitalWrite(7, HIGH);       // Set pull down signal
+    delayMicroseconds(30);   
+
+  
+   pinMode(7, INPUT);          // Set pin to safe input mode
     
     while( ! digitalRead(7)){   // wait until signal pulled up
       watchdog++;
@@ -46,17 +49,6 @@ void send_start(){               //Send 18ms pull down signal
 
     // If here, signal PUTED down again
     watchdog = 0;
-
-        while( ! digitalRead(7)){     // Wait for DHT release the signal again
-      wait_us(10);
-      watchdog++;
-    if( watchdog > 100){      // test if DHT commute corretly to 0
-        Serial.print("DHT don't begin to send data\n");
-        return;                  // else quit function with error
-         }
-    //DHT start next bit transmisson
-    while(digitalRead(7)); //synchro
-    }
 }
 
 
@@ -64,20 +56,22 @@ int measure_time(){
     int measure_loop = 0;               // init counting
    // timer1_flag = 0;                    // Sync the timer
    
-    while( ! digitalRead(7));           // Wait while DHT stop holding down the signal (50ms normally)
+    while( ! digitalRead(7));           // Wait while DHT stop holding down the signal (50us normally)
     
     while( digitalRead(7)){          // Wait while timer flag doesn't reach 20us
         wait_us(10);                    // Start counting 10us packets
         measure_loop ++;                // Count n*10us
-        if(measure_loop > 100){         // Arbitrary error-exit value
+        if(measure_loop > 1000){         // Arbitrary error-exit value
             Serial.print("Watchdog measure reached\n"); // Error message
             return -1;                                      // Quit function
                                }
                         }
     // Bits value determination
-    if ( (measure_loop <= 3) && (measure_loop != 0)){
+    //if ( (measure_loop <= 3) && (measure_loop != 0)){
+    if ( measure_loop <= 3){
         return 0;
-    } else if ((measure_loop > 3  ) && (measure_loop <= 9)){
+    } //else if ((measure_loop > 3  ) && (measure_loop <= 9)){
+      else if ((measure_loop > 3  ) && (measure_loop <= 7)){
         return 1;
     }else{
          Serial.print("Can't determine bit value\n"); // Error message
@@ -94,7 +88,8 @@ int receive(int * integral_RH, int * decimal_rh, int * integral_T, int * decimal
     int bit_received = 0;       // Last bit received
     int trame = 0;              // Constructing trame
 
-    
+    if(send_start() == -1) return -1;
+
     while(number_of_packets){
         trame = 0;          // RAZ trame
         packet_length = 8;
@@ -117,34 +112,31 @@ int receive(int * integral_RH, int * decimal_rh, int * integral_T, int * decimal
   return 0;
 }
 
-void loop(){
+void loop(){      // TODO dégager les valeurs en dehors du scope du capteur
 
-int lol = 0;
 int integral_RH = 0, decimal_rh = 0, integral_T = 0, decimal_T = 0, checksum = 0;
 
-while(lol <5){
+DDRB = DDRB | 0b00100000;
+
 Serial.print("Send start\n"); // Error message
-send_start();
+
 if(receive(&integral_RH, &decimal_rh, &integral_T, &decimal_T, &checksum) != -1){
-Serial.print("RH integral :");Serial.println(integral_RH, BIN);
-Serial.print("RH decimal :");Serial.println(decimal_rh, BIN);
-Serial.print("Integral T :");Serial.println(integral_T, BIN);
-Serial.print("Decimal T :");Serial.println(decimal_T, BIN);
+Serial.print("RH integral :");Serial.println(integral_RH);
+Serial.print("RH decimal :");Serial.println(decimal_rh);
+Serial.print("Integral T :");Serial.println(integral_T);
+Serial.print("Decimal T :");Serial.println(decimal_T);
 Serial.print("checksum :");Serial.println(checksum, BIN);
 }else{
   Serial.print("ERROR : received returned -1\n");
-  Serial.print("RH integral :");Serial.println(integral_RH, BIN);
-Serial.print("RH decimal :");Serial.println(decimal_rh, BIN);
-Serial.print("Integral T :");Serial.println(integral_T, BIN);
-Serial.print("Decimal T :");Serial.println(decimal_T, BIN);
-Serial.print("checksum :");Serial.println(checksum, BIN);
+  delay(1000);
 }
+delay(500);
 
-lol++;
-delay(1000);
-}
+PORTB = PORTB | 0b100000;
 
-return;
+delay(500);
+
+PORTB = PORTB & 0b11011111;
 
 /*//Serial.print("Send high\n"); // Error message
 digitalWrite(7, HIGH);
